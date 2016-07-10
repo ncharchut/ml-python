e47b19e0aa1fe664f588403f4f28311917e0e4cb
Nick Charchut <nicholas.charchut@gmail.com>
2016-07-10 14:16:21 -0400

    cleaned and commented

--- Gradient_Descent.py
+++ Gradient_Descent.py
@@ -1,6 +1,4 @@
 import numpy as np
-import scipy
-
 
 Xdata, ydata = [], []
 
@@ -12,30 +10,29 @@ with open('data/ex1data1.txt','rU') as f:
 
 X = np.array([Xdata]).transpose()
 y = np.array([ydata]).transpose()
-Theta = np.zeros((len()))
+Theta = np.zeros((len(X[0]),1))
 
 ############# USING NUMPY ################
 
 def mean_square_error(Theta, X, y):
 	m = len(y)
+	# J = 1/(2*m) * sum((X*Theta - y)^2)
 	return 1/(2.*m)*sum(np.square((np.subtract(X.dot(Theta),y))))
-
-print mean_square_error(Theta,X,y)
 
 def gradient_descent(Theta, X, y, alpha=0.0001, num_iterations=1000):
 	m = len(y)
 	J_cost = np.zeros((num_iterations,1))
 
 	for i in xrange(num_iterations):
+		# Theta = Theta - alpha/m * X'* (X * Theta - y)
 		Theta = np.subtract(Theta, alpha/m*X.transpose().dot(np.subtract(X.dot(Theta),y)))
 		J_cost[i] = mean_square_error(Theta, X, y)
 
 	return Theta, J_cost
 
-theta, J = gradient_descent(Theta, X, y)
+Theta, J = gradient_descent(Theta, X, y)
 
-print theta
+print Theta
 print J
-
 
 ##########################################
