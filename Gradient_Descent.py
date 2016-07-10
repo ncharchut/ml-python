1293d663638bfbbc68990e6f4c8e048f7586f3db
Nick Charchut <nicholas.charchut@gmail.com>
2016-07-10 19:51:28 -0400

    normalization added

--- Gradient_Descent.py
+++ Gradient_Descent.py
@@ -1,27 +1,33 @@
 import numpy as np
 
 Xdata, ydata = [], []
-
-with open('data/ex1data1.txt','rU') as f:
+with open('data/ex1data2.txt','rU') as f:
 	for row in f:
-		x, y1 = row.split(',')
-		Xdata.append(float(x))
+		x, x1, y1 = row.split(',')
+		Xdata.append([float(x),float(x1)])
 		ydata.append(float(y1))
 
-X = np.array([Xdata]).transpose()
+X = np.array(Xdata)
 y = np.array([ydata]).transpose()
-Theta = np.zeros((len(X[0]),1))
+Theta = np.zeros((len(X[0])+1,1))
 
 ############# USING NUMPY ################
+
+def normalize(X):
+	mu = np.mean(X, axis=0)
+	sigma = np.std(X, axis=0)
+	normalized_X = (X-mu)/sigma
+	return np.insert(normalized_X, 0, 1, axis=1)
 
 def mean_square_error(Theta, X, y):
 	m = len(y)
 	# J = 1/(2*m) * sum((X*Theta - y)^2)
 	return 1/(2.*m)*sum(np.square((np.subtract(X.dot(Theta),y))))
 
-def gradient_descent(Theta, X, y, alpha=0.0001, num_iterations=1000):
+def gradient_descent(Theta, X, y, alpha=0.01, num_iterations=10000):
 	m = len(y)
 	J_cost = np.zeros((num_iterations,1))
+	X = normalize(X)
 
 	for i in xrange(num_iterations):
 		# Theta = Theta - alpha/m * X'* (X * Theta - y)
