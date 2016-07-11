980ed9baf253402ccd723e778fb7d2d77166a9d8
Nick Charchut <nicholas.charchut@gmail.com>
2016-07-11 01:22:36 -0400

    best fit scatter plot

--- Gradient_Descent.py
+++ Gradient_Descent.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 Xdata, ydata = [], []
 with open('data/ex1data2.txt','rU') as f:
@@ -35,15 +36,26 @@ def gradient_descent(Theta, X, y, alpha=0.01, num_iterations=10000):
 		Theta = np.subtract(Theta, alpha/m*X.transpose().dot(np.subtract(X.dot(Theta),y)))
 		J_cost[i] = mean_square_error(Theta, X, y)
 
-	return Theta, J_cost
+	return Theta, J_cost, X
 
-Theta, J = gradient_descent(Theta, X, y)
-print Theta
-print J
+Theta, J, X = gradient_descent(Theta, X, y)
+# print Theta
+# print J
+print X.dot(Theta)
+print X
 
-plt.plot(range(10000),J)
-plt.xlabel('Number of Iterations')
-plt.ylabel('Cost J')
+# plt.scatter(X[:,0],y,color='red')
+plt.scatter(X[:,1],y,color='blue')
+plt.scatter(X[:,1], X.dot(Theta), color='red')
+# plt.title('Error over iterations')
+# plt.xlabel('Number of Iterations')
+# plt.ylabel('Cost J')
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+
+# ax.scatter(X[:,1],X[:,2],zs=y,color='blue')
+
+# ax.scatter(X[:,1],X[:,2], zs=Theta.transpose().dot(X.transpose()),color='red')
 plt.show()
 
 ##########################################
