3efda7c9e61088fd1448dfd2c671f8102e184ca2
Nick Charchut <nicholas.charchut@gmail.com>
2016-07-11 15:30:47 -0400

    binary data generator cleaned up

--- binary_data_generator.py
+++ binary_data_generator.py
@@ -7,35 +7,41 @@ import random
     Learning, Chapter 2"""
 
 
-m_0 = []
-m_1 = []
-mu_0 = [1,0]
-mu_1 = [0,1]
-cov = [[1,0],[0,1]]
+def sample_binary_data():
+    m_0 = []
+    m_1 = []
+    mu_0 = [1,0]
+    mu_1 = [0,1]
+    cov = [[1,0],[0,1]]
 
-for _ in xrange(10):
-    x, y = np.random.multivariate_normal(mu_0,cov,1).T
-    m_0.append([x[0],y[0]])
-    x, y = np.random.multivariate_normal(mu_1,cov,1).T
-    m_1.append([x[0],y[0]])
+    for _ in xrange(10):
+        x, y = np.random.multivariate_normal(mu_0,cov,1).T
+        m_0.append([x[0],y[0]])
+        x, y = np.random.multivariate_normal(mu_1,cov,1).T
+        m_1.append([x[0],y[0]])
 
-x_0, y_0 = [], []
-x_1, y_1 = [], []
-cov = [[1./5,0],[0,1./5]]
+    x_0, y_0 = [], []
+    x_1, y_1 = [], []
+    cov = [[1./5,0],[0,1./5]]
 
-np.append(x_0, [1])
+    for _ in xrange(100):
+        m_k = random.choice(m_0)
+        x, y =  np.random.multivariate_normal(m_k,cov,1).T
+        x_0.append(x[0])
+        y_0.append(y[0])
+        
+        m_k = random.choice(m_1)
+        x, y =  np.random.multivariate_normal(m_k,cov,1).T
+        x_1.append(x[0])
+        y_1.append(y[0])
 
-for _ in xrange(100):
-    m_k = random.choice(m_0)
-    x, y =  np.random.multivariate_normal(m_k,cov,1).T
-    x_0.append(x[0])
-    y_0.append(y[0])
-    
-    m_k = random.choice(m_1)
-    x, y =  np.random.multivariate_normal(m_k,cov,1).T
-    x_1.append(x[0])
-    y_1.append(y[0])
+    return (x_0,y_0),(x_1,y_1)
 
-data_0 = plt.plot(x_0,y_0,'o',color='blue',fillstyle='none')
-data_1 = plt.plot(x_1,y_1,'o',color='orange',fillstyle='none')
-plt.show()
+def plot_data(xs, ys):
+    assert len(xs) == len(ys) == 2
+    x_0, x_1 = xs
+    y_0, y_1 = ys
+    data_0 = plt.plot(x_0,y_0,'o',color='blue',fillstyle='none')
+    data_1 = plt.plot(x_1,y_1,'o',color='orange',fillstyle='none')
+    plt.show()
+    return data_0, data_1
